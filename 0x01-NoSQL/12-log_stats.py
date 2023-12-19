5a2fb65553f210ed5e5fcc4de53d72cf37898308
michaelmensa <michaelmensa@live.com>
2023-12-19 09:30:29 +0000

    task12: log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,7 +6,6 @@ stored in MongoDB
 from pymongo import MongoClient
 
 
-
 if __name__ == '__main__':
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
@@ -18,12 +17,11 @@ if __name__ == '__main__':
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    print("Methods:")
     for method in methods:
-        count = collection.count_documents({'method': method})
+        count = len(list(collection.find({'method': method})))
         print(f'\tmethod {method}: {count}')
 
-    status_path_count = collection.count_documents(
-            {'method': "GET", 'path': '/status'}
-            )
+    status_path_count = len(list(
+        collection.find({'method': 'GET', 'path': '/status'})
+        ))
     print(f'{status_path_count} status check')

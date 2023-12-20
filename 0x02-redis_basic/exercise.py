42cd20fd0c6fb49bc9fdf777ce0cc433cc8823d7
michaelmensa <michaelmensa@live.com>
2023-12-20 14:41:34 +0000

    task4i

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -33,7 +33,7 @@ def replay(func: Callable) -> None:
     outputs = history.lrange(o_key, 0, -1)
 
     for i, o in zip(inputs, outputs):
-        print(f'{func.__qualname}(*{i.decode("utf-8")}) -> {o}')
+        print(f'{func.__qualname__}(*{i.decode("utf-8")}) -> {o}')
 
 
 def count_calls(method: Callable) -> Callable:
